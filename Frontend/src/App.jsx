a891eefa2936f62e8c4fc51bf599251ba6be5181
inferal <pyurik44@gmail.com>
2025-02-04 16:42:03 +0300

    Подключение бекенда и фронтенда

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,49 @@
-function App() {
+import React, {useEffect} from 'react';
+import { Menu } from 'antd';
+import axios from "axios";
+const items = [
+ {
+     key: 'g1',
+     label: 'Список криптовалют',
+     type: 'group',
+     children: [
+       {
+         key: '1',
+         label: 'Option 1',
+       },
+     {
+       key: '2',
+       label: 'Option 2',
+     },
+   ],
+ },
+];
+const App = () => {
 
-  return (
-    <>
-        Hello
-    </>
-  )
-}
+    const fetchCurrencies = () => {
+        axios.get('http://127.0.0.1:8000/cryptocurrencies').then(r => {
+            console.log('r',r)
+        })
+    }
 
-export default App
+    useEffect(() => {
+        fetchCurrencies()
+    }, []);
+
+    const onClick = (e) => {
+        console.log('click ', e);
+    };
+    return (
+        <Menu
+            onClick={onClick}
+            style={{
+                width: 256,
+            }}
+            defaultSelectedKeys={['1']}
+            defaultOpenKeys={['sub1']}
+            mode="inline"
+            items={items}
+        />
+    );
+};
+export default App;
